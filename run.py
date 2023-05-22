d621ac1aa4bbef9959237dcf30da189a1807e096
Owen Brooks <owen.h.brooks@gmail.com>
2023-05-22 13:43:04 +1000

    Add function documentation

--- run.py
+++ run.py
@@ -8,13 +8,26 @@ PROJECT_NAME="ur5-noetic"
 DOCKERFILE="ur5/Dockerfile.noetic"
 
 def build_image(image_name: str, dockerfile: str):
+    """ Build an image from the supplied dockerfile. Uses the current directory as the docker context. """
     subprocess.run(["docker", "build", ".", "-f", dockerfile, "-t", image_name])
 
 def create_container(image_name: str):
+    """ Use 'docker run' to create a container.
+    
+    - GUI application support is enabled by setting the DISPLAY environment variable, and sharing the .X11-unix socket as a volume.
+    - External device support is enabled by sharing /dev as a volume.
+    - Full networking support is enabled by setting network mode to 'host' and enabling the 'privileged' flag.
+    - Loading of kernel modules from inside the container is enabled by sharing /lib/modules as a volume.
+    - Realtime scheduling support is enabled by setting ulimits
+    - The ROS distro is sourced by the ros_entrypoint.sh script supplied in the upstream docker images.
+    - The current working directory is shared as a volume inside the container.
+    - The container is started in the background, so that we can later attach to it from multiple terminals using 'docker exec'.
+    """
     run_command = f"""docker run -it \
         -e DISPLAY \
         --volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
         --network=host \
+        --privileged \
         --volume "/dev:/dev:rw" \
         --volume "/lib/modules:/lib/modules:rw" \
         --ulimit rtprio=99 \
@@ -30,6 +43,7 @@ def create_container(image_name: str):
     subprocess.run(run_command, shell=True)
 
 def attach_to_container(container_name: str):
+    """ Attach the terminal to an existing container, starting it if it is currently in stopped state. """
     # Allow container to create GUI windows on the host's X server
     subprocess.run("xhost + >> /dev/null", shell=True)
     # Start the container in case it has been stopped
@@ -39,22 +53,26 @@ def attach_to_container(container_name: str):
     subprocess.run(f"docker exec -it {container_name} {starting_command}", shell=True)
 
 def remove_container(container_name: str):
+    """ Delete the container. """
     remove_container_command = f"docker rm -f {container_name}"
     print(remove_container_command)
     subprocess.run(remove_container_command, shell=True)
 
 def remove_image(image_name: str):
+    """ Delete the image. """
     remove_image_command = f"docker rmi -f {image_name}"
     print(remove_image_command)
     subprocess.run(remove_image_command, shell=True)
 
 def image_exists(image_name: str) -> bool:
+    """ Check if an image with the specified name has previously been built """
     image_list_command = f"docker images -f reference={image_name} -q"
     output = subprocess.run(image_list_command, stdout=subprocess.PIPE, shell=True).stdout.decode() # run the command as if in a shell, capture stdout
     already_exists = len(output) > 0
     return already_exists
 
 def container_exists(container_name: str) -> bool:
+    """ Check if an image with the specified name has previously been created. It can be in stopped or running state. """
     container_list_command = f"docker ps -qa -f name={container_name}"
     output = subprocess.run(container_list_command, stdout=subprocess.PIPE, shell=True).stdout.decode() # run the command as if in a shell, capture stdout
     already_exists = len(output) > 0
