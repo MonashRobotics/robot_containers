d373bb5260018b3911acf7b7e7a0db49fc55d2fa
Owen Brooks <owen.h.brooks@gmail.com>
2023-05-24 12:45:07 +1000

    Add ruff and black linting and formatting to CI (#1)

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ DOCKERFILE = "./Dockerfile"
 
 
 def build_image(image_name: str, dockerfile: str):
-    """ 
-    Build an image from the supplied dockerfile. 
+    """
+    Build an image from the supplied dockerfile.
 
     Uses the current directory as the docker context.
     """
@@ -21,18 +21,18 @@ def create_container(image_name: str):
     """
     Use 'docker run' to create a container.
 
-    - GUI application support is enabled by setting the DISPLAY environment variable, 
+    - GUI application support is enabled by setting the DISPLAY environment variable,
     and sharing the .X11-unix socket as a volume.
     - External device support is enabled by sharing /dev as a volume.
-    - Full networking support is enabled by setting network mode to 'host' and 
+    - Full networking support is enabled by setting network mode to 'host' and
     enabling the 'privileged' flag.
-    - Loading of kernel modules from inside the container is enabled by sharing 
+    - Loading of kernel modules from inside the container is enabled by sharing
     /lib/modules as a volume.
     - Realtime scheduling support is enabled by setting ulimits
-    - The ROS distro is sourced by the ros_entrypoint.sh script supplied in the 
+    - The ROS distro is sourced by the ros_entrypoint.sh script supplied in the
     upstream docker images.
     - The current working directory is shared as a volume inside the container.
-    - The container is started in the background, so that we can later attach 
+    - The container is started in the background, so that we can later attach
     to it from multiple terminals using 'docker exec'.
     """
     run_command = f"""docker run -it \
@@ -57,7 +57,7 @@ def create_container(image_name: str):
 
 def attach_to_container(container_name: str):
     """
-    Attach the terminal to an existing container, starting it if it is currently in 
+    Attach the terminal to an existing container, starting it if it is currently in
     stopped state.
     """
     # Allow container to create GUI windows on the host's X server
@@ -97,8 +97,8 @@ def image_exists(image_name: str) -> bool:
 
 def container_exists(container_name: str) -> bool:
     """
-    Check if an image with the specified name has previously been created. 
-    
+    Check if an image with the specified name has previously been created.
+
     It can be in stopped or running state.
     """
     container_list_command = f"docker ps -qa -f name={container_name}"
