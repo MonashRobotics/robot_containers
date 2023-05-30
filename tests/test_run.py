72ccdc60af8378c4bb821092526358f0f2d7ab84
Owen Brooks <owen.h.brooks@gmail.com>
2023-05-30 15:09:44 +1000

    Remove unused test code

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,8 +1,7 @@
 import logging
 from pathlib import Path
 
-from roboco.configurations import ProjectConfiguration, realsense_camera, ur5
-from roboco.run import attach_to_container, build_image, create_container, remove_container, remove_image
+from roboco.run import build_image
 
 
 def test_build_image():
@@ -10,9 +9,3 @@ def test_build_image():
     log.setLevel(logging.DEBUG)
     build_successful = build_image("test", "tests/Dockerfile.minimal", Path("."))
     assert build_successful
-
-# def test_create_container():
-#     log = logging.getLogger()
-#     log.setLevel(logging.DEBUG)
-#     create_successful = create_container("test", "test")
-#     assert create_successful
