57379eac7b309647cfdf8b5bce2602d4b5501fb1
Owen Brooks <owen.h.brooks@gmail.com>
2023-06-06 09:14:35 +1000

    Use exceptions for checking if errors occurred

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,11 +1,17 @@
 import logging
 from pathlib import Path
 
-from roboco.run import build_image
+from roboco.run import build_image, remove_image
 
 
 def test_build_image():
     log = logging.getLogger()
     log.setLevel(logging.DEBUG)
-    build_successful = build_image("test", "tests/Dockerfile.minimal", Path("."))
-    assert build_successful
+    build_image("test", "tests/Dockerfile.minimal", Path("."))
+
+
+def test_remove_image():
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
+    build_image("test", "tests/Dockerfile.minimal", Path("."))
+    remove_image("test")
